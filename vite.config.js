f80083f8f6fc8a034fae6233a4fe666c6ec0ce6d
flx-sta <50131232+flx-sta@users.noreply.github.com>
2026-07-06 13:25:19 -0700

    feat: sync extension manifest version with package.json during build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import { crx } from "@crxjs/vite-plugin";
 import zipPack from "vite-plugin-zip-pack";
-import manifest from "./src/manifest.json";
+import { readFileSync } from "fs";
+import pkg from "./package.json";
+
+const manifest = JSON.parse(
+  readFileSync(new URL("./src/manifest.json", import.meta.url), "utf-8"),
+);
+// const pkg = JSON.parse(
+//   readFileSync(new URL("./package.json", import.meta.url), "utf-8"),
+// );
+
+manifest.version = pkg.version;
 
 export default defineConfig({
   root: "src",
